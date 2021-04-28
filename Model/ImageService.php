a7404df4413eb63ce530bcc180ba7b0577844a83
Mediacamp <dev@mediacamp.pl>
2021-04-28 11:45:55 +0200

    Add image position to export data

--- Model/ImageService.php
+++ Model/ImageService.php
@@ -183,6 +183,7 @@ class ImageService
         // if product is configurable then first will be parent and others are children
         foreach ($products as $k => $product) {
             $isMainProduct = !$k;
+            $imgPosition = 0;
 
             foreach ($product->getMediaGalleryImages() as $productGalleryImage) {
                 $isFound = false;
@@ -194,6 +195,13 @@ class ImageService
 
                         if ($image->getImageData()) {
                             $savedImageData = json_decode($image->getImageData(), true);
+
+                            if ($product->getThumbnail() == $productGalleryImage->getFile()) {
+                                $savedImageData['position'] = 0;
+                            } else {
+                                $imgPosition++;
+                                $savedImageData['position'] = $imgPosition;
+                            }
 
                             if ($isMainProduct) {
                                 $imagesData['main'][] = $savedImageData;
