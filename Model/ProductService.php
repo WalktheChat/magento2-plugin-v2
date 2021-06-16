9aa63061660f84eebdf05be28b2e73011abb0ef3
Mediacamp <dev@mediacamp.pl>
2021-06-16 22:12:16 +0000

    JWK-35 Use MSI to calculate available qty

--- Model/ProductService.php
+++ Model/ProductService.php
@@ -28,57 +28,65 @@ class ProductService
     protected $searchCriteriaBuilder;
 
     /**
-     * @var \Magento\CatalogInventory\Model\Stock\StockItem
+     * @var \Magento\InventorySalesApi\Api\StockResolverInterface
      */
-    protected $stockItem;
+    protected $stockResolver;
+
+    /**
+     * @var \Magento\InventorySalesApi\Api\GetProductSalableQtyInterface
+     */
+    protected $getProductSalableQty;
 
     /**
      * @var \Walkthechat\Walkthechat\Helper\Data
      */
     protected $helper;
-	
+
     /**
      * @var \Walkthechat\Walkthechat\Model\QueueService
      */
     private $queueService;
-	
+
     /**
      * @var \Walkthechat\Walkthechat\Api\QueueRepositoryInterface
      */
     private $queueRepository;
-	
-	/**
+
+    /**
      * @var \Magento\CatalogRule\Model\RuleFactory
      */
-	private $ruleFactory;
+    private $ruleFactory;
 
     /**
      * ProductService constructor.
      *
-     * @param \Magento\Catalog\Model\ProductRepository                  $productRepository
-     * @param \Magento\Framework\Api\SearchCriteriaBuilder              $searchCriteriaBuilder
+     * @param \Magento\Catalog\Model\ProductRepository                      $productRepository
+     * @param \Magento\Framework\Api\SearchCriteriaBuilder                  $searchCriteriaBuilder
      * @param \Walkthechat\Walkthechat\Helper\Data                          $helper
-     * @param \Magento\CatalogInventory\Api\StockStateInterface         $stockItem
+     * @param \Magento\InventorySalesApi\Api\StockResolverInterface         $stockResolver
+     * @param \Magento\InventorySalesApi\Api\GetProductSalableQtyInterface  $getProductSalableQty
      * @param \Walkthechat\Walkthechat\Model\QueueService                   $queueService
      * @param \Walkthechat\Walkthechat\Api\QueueRepositoryInterface         $queueRepository
-     * @param \Magento\CatalogRule\Model\RuleFactory								$ruleFactory
+     * @param \Magento\CatalogRule\Model\RuleFactory					    $ruleFactory
      */
     public function __construct(
         \Magento\Catalog\Model\ProductRepository $productRepository,
         \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
         \Walkthechat\Walkthechat\Helper\Data $helper,
-        \Magento\CatalogInventory\Api\StockStateInterface $stockItem,
+        \Magento\InventorySalesApi\Api\StockResolverInterface $stockResolver,
+        \Magento\InventorySalesApi\Api\GetProductSalableQtyInterface $getProductSalableQty,
         \Walkthechat\Walkthechat\Model\QueueService $queueService,
         \Walkthechat\Walkthechat\Api\QueueRepositoryInterface $queueRepository,
-		\Magento\CatalogRule\Model\RuleFactory $ruleFactory
+        \Magento\CatalogRule\Model\RuleFactory $ruleFactory
     ) {
         $this->productRepository     = $productRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->stockItem             = $stockItem;
+        $this->stockResolver         = $stockResolver;
+        $this->getProductSalableQty  = $getProductSalableQty;
         $this->helper                = $helper;
         $this->queueService          = $queueService;
         $this->queueRepository       = $queueRepository;
-        $this->ruleFactory       = $ruleFactory;
+        $this->ruleFactory           = $ruleFactory;
     }
 
     /**
@@ -295,23 +303,24 @@ class ProductService
      */
     public function prepareProductData($product, $isNew = true, array $imagesData = [], array $mediaContentData = [])
     {
-		$rule =  $this->ruleFactory->create();
-		
+        $rule =  $this->ruleFactory->create();
+
+        $stockId          = $this->stockResolver->execute(\Magento\InventorySalesApi\Api\Data\SalesChannelInterface::TYPE_WEBSITE, $this->helper->getWebsite()->getCode())->getStockId();
         $mainPrice        = $this->helper->convertPrice($product->getPrice());
         $mainSpecialPrice = null;
         if (!$product->getSpecialFromDate() && !$product->getSpecialToDate()) {
             $mainSpecialPrice = $this->helper->convertPrice($product->getSpecialPrice());
         }
-		$mainRulePrice = $this->helper->convertPrice($rule->calcProductPriceRule($product, $product->getPrice()));
-		
-		if (($mainRulePrice && !$mainSpecialPrice) || ($mainRulePrice && $mainRulePrice < $mainSpecialPrice)) {
-			$mainSpecialPrice = $mainRulePrice;
-		}
+        $mainRulePrice = $this->helper->convertPrice($rule->calcProductPriceRule($product, $product->getPrice()));
 
-		if ($mainPrice == $mainSpecialPrice) {
-		    $mainSpecialPrice = null;
+        if (($mainRulePrice && !$mainSpecialPrice) || ($mainRulePrice && $mainRulePrice < $mainSpecialPrice)) {
+            $mainSpecialPrice = $mainRulePrice;
         }
-		
+
+        if ($mainPrice == $mainSpecialPrice) {
+            $mainSpecialPrice = null;
+        }
+
         $productVisibility =
             $product->getVisibility() != \Magento\Catalog\Model\Product\Visibility::VISIBILITY_NOT_VISIBLE
             && !$product->isDisabled();
@@ -325,7 +334,7 @@ class ProductService
             'variants'              => [
                 [
                     'id'                => $product->getId(),
-                    'inventoryQuantity' => $this->stockItem->getStockQty($product->getId()),
+                    'inventoryQuantity' => $this->_getProductSaleableQty($product->getSku(), $stockId),
                     'weight'            => $product->getWeight(),
                     'requiresShipping'  => true,
                     'sku'               => $product->getSku(),
@@ -375,29 +384,29 @@ class ProductService
 
                 $k = 0;
                 foreach ($children as $child) {
-					$childPrice			= $this->helper->convertPrice($child->getPrice());
+                    $childPrice			= $this->helper->convertPrice($child->getPrice());
                     $childSpecialPrice = null;
                     if (!$child->getSpecialFromDate() && !$child->getSpecialToDate()) {
                         $childSpecialPrice = $this->helper->convertPrice($child->getSpecialPrice());
                     }
-					$childRulePrice = $this->helper->convertPrice($rule->calcProductPriceRule($child, $child->getPrice()));
-		
-					if (($childRulePrice && !$childSpecialPrice) || ($childRulePrice && $childRulePrice < $childSpecialPrice)) {
-						$childSpecialPrice = $childRulePrice;
-					}
+                    $childRulePrice = $this->helper->convertPrice($rule->calcProductPriceRule($child, $child->getPrice()));
 
-					if ($childPrice == $childSpecialPrice) {
-					    $childSpecialPrice = null;
+                    if (($childRulePrice && !$childSpecialPrice) || ($childRulePrice && $childRulePrice < $childSpecialPrice)) {
+                        $childSpecialPrice = $childRulePrice;
+                    }
+
+                    if ($childPrice == $childSpecialPrice) {
+                        $childSpecialPrice = null;
                     }
 
                     $data['variants'][$k] = [
                         'id'                => $child->getId(),
-                        'inventoryQuantity' => $this->stockItem->getStockQty($child->getId()),
+                        'inventoryQuantity' => $this->_getProductSaleableQty($child->getSku(), $stockId),
                         'weight'            => $child->getWeight(),
                         'requiresShipping'  => true,
                         'sku'               => $child->getSku(),
                         'price'             => $childSpecialPrice ? $childSpecialPrice : $childPrice,
-						'compareAtPrice'    => $childSpecialPrice ? $childPrice : null,
+                        'compareAtPrice'    => $childSpecialPrice ? $childPrice : null,
                         'visibility'        => $child->isDisabled() ? false : true,
                         'taxable'           => (bool)$child->getTaxClassId(),
                     ];
@@ -436,5 +445,20 @@ class ProductService
         }
 
         return $data;
+    }
+
+    /**
+     * @param $productSku
+     * @param $stockId
+     * @return int
+     */
+    private function _getProductSaleableQty($productSku, $stockId)
+    {
+        try{
+            $qty = $this->getProductSalableQty->execute($productSku, $stockId);
+        } catch(Exception $exception){
+            $qty = 0;
+        }
+        return $qty;
     }
 }
